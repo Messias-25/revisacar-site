424435b505e0caafa00f360056b25d843acce1a0
Messias-25 <dyrfbb9drm@privaterelay.appleid.com>
2026-07-07 03:26:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("RevisaCar carregado com sucesso!");
+document.addEventListener("DOMContentLoaded", function(){
+
+    console.log("Site RevisaCar carregado com sucesso!");
+
+    const cards = document.querySelectorAll(".card, .info-card");
+
+    cards.forEach(card => {
+        card.addEventListener("mouseenter", () => {
+            card.style.transition = "0.3s";
+        });
+    });
+
+    const whatsapp = document.querySelector(".whatsapp");
+
+    if(whatsapp){
+        whatsapp.addEventListener("click", function(){
+            console.log("Cliente direcionado para o WhatsApp");
+        });
+    }
+
+});
